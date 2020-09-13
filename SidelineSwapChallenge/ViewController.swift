ce119fd421e2c89d474f96bb44e0836139572fe9
Gershy Lev <gbay9855@yahoo.com>
2020-09-13 12:39:20 -0700

    Initial Commit

---
+++ SidelineSwapChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SidelineSwapChallenge
+//
+//  Created by Gershy Lev on 9/13/20.
+//  Copyright © 2020 Gershy Lev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
